7d66d311424f6b834e3d22f218869e693e80ef6f
jiangzenghui005@163.com <jiangzenghui005@163.com>
2018-08-16 17:16:49 +0800

    Initial Commit

---
+++ TestRACUnarySequence/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRACUnarySequence
+//
+//  Created by ys on 2018/8/16.
+//  Copyright © 2018年 ys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
