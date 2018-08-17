3591463e5515ba2ad3f8a10064d25613102193da
jiangzenghui005@163.com <jiangzenghui005@163.com>
2018-08-17 18:35:47 +0800

    测试用例提交

--- TestRACUnarySequenceTests/TestRACUnarySequenceTests.m
+++ TestRACUnarySequenceTests/TestRACUnarySequenceTests.m
@@ -8,32 +8,51 @@
 
 #import <XCTest/XCTest.h>
 
+#import <ReactiveCocoa.h>
+#import <RACUnarySequence.h>
+
 @interface TestRACUnarySequenceTests : XCTestCase
 
 @end
 
 @implementation TestRACUnarySequenceTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)test_return
+{
+    RACUnarySequence *sequence = [RACUnarySequence return:@"xxx"];
+    NSLog(@"return -- %@", sequence);
+    
+    // 打印日志：
+    /*
+     2018-08-16 17:21:03.940582+0800 TestRACUnarySequence[15973:17015192] return -- <RACUnarySequence: 0x600000238820>{ name = , head = xxx }
+     */
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)test_tail
+{
+    RACUnarySequence *sequence = [RACUnarySequence return:@[@1, @2, @3]];
+    NSLog(@"tail -- %@", sequence.tail);
+    
+    // 打印日志：
+    /*
+     2018-08-16 17:22:49.841689+0800 TestRACUnarySequence[16078:17019928] tail -- (null)
+     */
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void)test_bind
+{
+    RACUnarySequence *sequence = [RACUnarySequence return:@(1)];
+    RACSequence *s = [sequence bind:^RACStreamBindBlock{
+        return ^(id value, BOOL *stop) {
+            return [RACUnarySequence return:@(100)];
+        };
     }];
+    NSLog(@"bind -- %@", s);
+    
+    // 打印日志：
+    /*
+     2018-08-16 17:25:01.532451+0800 TestRACUnarySequence[16176:17026442] bind -- <RACUnarySequence: 0x6000002341c0>{ name = , head = 100 }
+     */
 }
 
 @end
